71bfd5f51be3e138f9ad3d886fa42f926f132c1e
Jorge Ramirez <joramirez96@gmail.com>
2020-04-13 23:09:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    # index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
-    # html = open(index_url)
-    # index = Nokogiri::HTML(html)
+    
+    
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    binding.pry
     # student name (h4 class="student-name")
-  end
-
+  end 
 
 
 
