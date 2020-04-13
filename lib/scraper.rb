c670c9343655d6804fc61aaf743733af750ab3cd
Jorge Ramirez <joramirez96@gmail.com>
2020-04-13 22:29:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,29 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    # index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
+    # html = open(index_url)
+    # index = Nokogiri::HTML(html)
+    # student name (h4 class="student-name")
   end
+
+
+
+
+
 
   def self.scrape_profile_page(profile_url)
-    
+
+
+
   end
 
-end
 
+
+
+
+end
